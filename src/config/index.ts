6fd0152190e0dc5aab9ac2ffa34832e03d2c5fc1
Ilia Liubinskii <ilia.liubinskii@gmail.com>
2024-08-19 02:20:49 +0300

    fix: For Jest not recognizing `import.meta.env`

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,5 +1,7 @@
 import { cleanEnv, str } from "envalid";
 
-export const { VITE_API_URL: API_URL } = cleanEnv(import.meta.env, {
-  VITE_API_URL: str({ default: "http://localhost:3000" })
-});
+export const { VITE_API_URL: API_URL } =
+  // eslint-disable-next-line no-process-env -- Ok
+  cleanEnv(process.env, {
+    VITE_API_URL: str({ default: "http://localhost:3000" })
+  });
--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,9 @@
 /* eslint-disable import/no-extraneous-dependencies -- Ok */
 
 import { defineConfig } from "vite";
+import environment from "vite-plugin-environment";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  plugins: [react()]
+  plugins: [environment(["VITE_API_URL"]), react()]
 });
