f49ef7036d9fa7268f54d98e1127fd9b53ddae5c
Ilia Liubinskii <ilia.liubinskii@gmail.com>
2024-08-18 18:50:30 +0300

    fix: Lang file texts

--- src/lang/index.ts
+++ src/lang/index.ts
@@ -1,11 +1,11 @@
 export const lang = {
   Add: "Add",
-  EnterUrl: "Enter an URL",
+  EnterUrl: "Enter a URL",
   MetadataExtractionTool: "Metadata Extraction Tool",
   RemoveUrl: "Remove URL",
   ServiceIsTemporarilyUnavailable:
-    "Service is temporarily unavailable. Please try again later.",
+    "The service is temporarily unavailable. Please try again later.",
   Submit: "Submit",
   instructions:
-    "Add or remove URLs using the input zone below. Submit the form to extract metadata for the selected URLs."
+    "Add or remove URLs using the input area below. Submit the form to extract metadata from the selected URLs."
 } as const;
